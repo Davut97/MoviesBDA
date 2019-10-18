99f59a9ad804698d930081090f2c36462840c33d
Davut97 <aydindavut14@gmail.com>
2019-10-18 22:41:45 +0300

    Search function and saving titles

--- ss.py
+++ ss.py
@@ -11,13 +11,22 @@ while (pages <= 10):
     url2 = urlTemplate.substitute(pages=pages)
     response = requests.get(url2)
     data = json.loads(response.text)
-    arrT += SearchFunction.extract_element_from_json(data, ['results', 'title'])  # array of titles
+    arrT += SearchFunction.extract_element_from_json(data, ['results', 'id'])  # array of titles
     for i in arrT:
          print(i)
     pages = pages+1
 print(len(arrT))
+id=arrT[0]
+print(id)
+urlDiscoveries= 'https://api.themoviedb.org/3/movie/$id?api_key=9902b134582ad4ddad59aa7e54a5164f'
+urlTemplate2 = Template(urlDiscoveries)
+urlDiscoveries2=urlTemplate2.substitute(id =id)
+response = requests.get(urlDiscoveries)
+data2 = json.loads(response.text)
+print(data2)
 for i in arrT:
-    with open('Movies-Titles.csv', 'a') as csvFile:
+
+    with open('Movies-ID.csv', 'a') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow([i])
     csvFile.close()
