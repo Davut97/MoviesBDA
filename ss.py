5f33a4e8d48ecc8dc2eb832f5546574fd9bdffbc
Davut97 <aydindavut14@gmail.com>
2019-10-19 12:32:41 +0300

    Search function and saving titles

--- ss.py
+++ ss.py
@@ -3,30 +3,18 @@ import  json
 import  SearchFunction
 from string import Template
 import  csv
-pages = 1
-url = 'https://api.themoviedb.org/3/discover/movie?api_key=9902b134582ad4ddad59aa7e54a5164f&page=$pages'
-urlTemplate = Template(url)
-arrT = []
-while (pages <= 10):
-    url2 = urlTemplate.substitute(pages=pages)
-    response = requests.get(url2)
-    data = json.loads(response.text)
-    arrT += SearchFunction.extract_element_from_json(data, ['results', 'id'])  # array of titles
-    for i in arrT:
-         print(i)
-    pages = pages+1
-print(len(arrT))
-id=arrT[0]
-print(id)
-urlDiscoveries= 'https://api.themoviedb.org/3/movie/$id?api_key=9902b134582ad4ddad59aa7e54a5164f'
-urlTemplate2 = Template(urlDiscoveries)
-urlDiscoveries2=urlTemplate2.substitute(id =id)
-response = requests.get(urlDiscoveries)
-data2 = json.loads(response.text)
-print(data2)
-for i in arrT:
 
-    with open('Movies-ID.csv', 'a') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerow([i])
-    csvFile.close()
+def findid():
+    pages = 1
+    url = 'https://api.themoviedb.org/3/discover/movie?api_key=9902b134582ad4ddad59aa7e54a5164f&page=$pages'
+    urlTemplate = Template(url)
+    arrID=[]                    # initiliaz the IDs array
+    while (pages <= 2):
+        url2 = urlTemplate.substitute(pages=pages)
+        response = requests.get(url2)
+        data = json.loads(response.text)
+        arrID += SearchFunction.extract_element_from_json(data, ['results', 'id'])  # array of titles
+        pages = pages+1
+    print(len(arrID))
+    return arrID
+
