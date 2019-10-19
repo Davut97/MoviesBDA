9303f76b029081bc4cba4c6b2f0da061922be541
Davut97 <aydindavut14@gmail.com>
2019-10-19 15:48:32 +0300

    Final version

--- WriteIntoCSV.py
+++ WriteIntoCSV.py
@@ -2,12 +2,16 @@ from string import Template
 import requests
 import  json
 import  csv
-from ss import findid
+
+from GenerateRandomMovies import findid
 def writeArrayToCsv(FileName,data):                                     # write into the csv file
     with open(FileName, 'w', newline='',encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         for row in data:
             writer.writerow(row)
+
+
+
 
 
 urlDiscoveries= 'https://api.themoviedb.org/3/movie/$id?api_key=9902b134582ad4ddad59aa7e54a5164f'
@@ -16,12 +20,24 @@ csvData=[["Title","Budget","Revenue"]]
 arrID2 =findid()
 for i in arrID2:
     id = i
+
     urlDiscoveries2=urlTemplate2.substitute(id =id)
     response = requests.get(urlDiscoveries2)
     data2 = json.loads(response.text)
-    title = data2['original_title']
-    budget = data2['budget']
-    revenue = data2['revenue']
+
+    try:
+        title = data2['original_title']
+        print(data2['original_title'])
+    except:
+        title=["NA"]
+    try:
+        budget = data2['budget']
+    except:
+        budget= [""]
+    try:
+        revenue = data2['revenue']
+    except:
+        revenue = [""]
     csvData.append([title,budget,revenue])
 
 writeArrayToCsv('Movies.csv',csvData)
